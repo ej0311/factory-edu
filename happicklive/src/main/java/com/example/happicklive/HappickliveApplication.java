1d752842266c9a55e22b6703e333c3e43c89d504
ejchoi <ejchoi@ejchois-MacBook-Pro.local>
2021-04-07 15:01:34 +0900

    add happicklive

---
+++ happicklive/src/main/java/com/example/happicklive/HappickliveApplication.java
@@ -0,0 +1,13 @@
+package com.example.happicklive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappickliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappickliveApplication.class, args);
+	}
+
+}
---
+++ happicklive/src/test/java/com/example/happicklive/HappickliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happicklive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappickliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
